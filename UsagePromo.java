f041386838dd2574ba7db6318774230f96ca49b0
Reirinn <142465054+Reirinn@users.noreply.github.com>
2024-05-23 01:26:02 +0000

    /commit

--- UsagePromo.java
+++ UsagePromo.java
@@ -1,4 +1,6 @@
 public interface UsagePromo {
-    public String showAllowance(String telcoName, double money);
-
+   
+        public String showAllowance(String telcoName, double money);
+        public String showAllowance(TelcoSubscription telco);
+    
 }
